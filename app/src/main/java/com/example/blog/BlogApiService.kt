4a42ded598aa6322e6d75083365dc83448722cba
Prem Sagar Rathor <prem.s.rathor01@gmail.com>
2024-12-24 11:47:17 +0530

    Delete app directory

--- app/src/main/java/com/example/blog/BlogApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.blog
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface BlogApiService {
-    @GET("todos") 
-    fun getBlogPosts(): Call<List<BlogPost>>
-}
--- app/src/main/java/com/example/blog/BlogPost.kt
+++
@@ -1,9 +0,0 @@
-package com.example.blog
-
-
-
-data class BlogPost(
-    val id: String,
-    val title: String,
-
-    )
